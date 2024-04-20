d349abfe15c43eeb7438ee296d687abf6cb89ac5
ksunchikm1111 <166916284+ksunchikm1111@users.noreply.github.com>
2024-04-20 13:44:17 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+let tg = window.Telegram.WebApp;
+
+tg.expand();
+
+let button = document.getElementById('click');
+
+button.onclick = () => {
+    if (tg.MainButton.isVisible) {
+        tg.MainButton.hide();
+    }
+    else {
+        tg.MainButton.setText("Вы нажали на кнопку!");
+        tg.MainButton.show();
+    }
+}
